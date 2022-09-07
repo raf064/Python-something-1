819f163887526503b8cb3a77ab12b3c2db097e1f
raf064 <ara22u418@student.bmstu.ru>
2022-09-07 23:42:17 +0300

    1

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ def print_hi(name):
     print(f'Hi, {name}')
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == 'main':
+    for i in range(100):
+        for j in range(100):
+            print(i,j)
+        print()
