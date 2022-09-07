d1a9ca19cb8425ecff5565143123d5307a3daff4
raf064 <ara22u418@student.bmstu.ru>
2022-09-07 23:51:31 +0300

    1

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def print_hi(name):
     print(f'Hi, {name}')
 
 
+def print_reversed(stroke):
     print(stroke[::-1])
+
+
 if __name__ == 'main':
     for i in range(100):
         for j in range(100):
